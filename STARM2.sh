c7b5625a8e1187175105789f44cc66656e4d7e8d
peterjgiles <79471080+peterjgiles@users.noreply.github.com>
2021-11-12 16:56:48 +0000

    Update STARM2.sh

--- STARM2.sh
+++ STARM2.sh
@@ -20,22 +20,22 @@ echo ${OUTPUT}
 echo ${GENCODE}
 
 STAR \
-    --genomeDir "$STARgdir" \
-    --readFilesCommand zcat \
-    --readFilesIn "$STARgdir/reads/${SAMPLE}_M_F.fq.gz" "$STARgdir/reads/${SAMPLE}_M_R.fq.gz" \
-    --chimSegmentMin 15 \
-    --outFilterMultimapNmax 1 \
-    --twopassMode Basic \
-    --runThreadN 8 \
-    --outSAMtype BAM SortedByCoordinate \
-    --outFileNamePrefix "$STARgdir/Mapped/${SAMPLE}_map"
+  --readFilesCommand zcat \
+  --twopassMode Basic \
+  --runThreadN 8 \
+  --chimSegmentMin 15 \
+  --outFilterMultimapNmax 1 \
+  --outSAMtype BAM SortedByCoordinate \
+  --readFilesIn "$STARgdir/reads/${SAMPLE}_M_F.fq.gz" "$STARgdir/reads/${SAMPLE}_M_R.fq.gz" \
+  --genomeDir "$STARgdir" \
+  --outFileNamePrefix "$STARgdir/Mapped/${SAMPLE}_map"
 
 
-${singjob}/./featureCounts -p -s 2 --donotsort -B -t "exon" -g "transcript_id" -a ${STARgdir}/${GENCODE} -o $OUTPUT/STAR/Mapped/${SAMPLE}.tc.out $STARgdir/Mapped/${SAMPLE}_mapAligned.sortedByCoord.out.bam && cut -f1,7 "$OUTPUT/STAR/Mapped/${SAMPLE}.tc.out" > $OUTPUT/STAR/Mapped/${SAMPLE}.tc.out.tab; sed -i 1,2d "$OUTPUT/STAR/Mapped/${SAMPLE}.tc.out.tab"
+${singjob}/featureCounts -p -s 2 --donotsort -B -t "exon" -g "transcript_id" -a ${STARgdir}/${GENCODE} -o $OUTPUT/STAR/Mapped/${SAMPLE}.tc.out $STARgdir/Mapped/${SAMPLE}_mapAligned.sortedByCoord.out.bam && cut -f1,7 "$OUTPUT/STAR/Mapped/${SAMPLE}.tc.out" > $OUTPUT/STAR/Mapped/${SAMPLE}.tc.out.tab; sed -i 1,2d "$OUTPUT/STAR/Mapped/${SAMPLE}.tc.out.tab"
 
-${singjob}/./featureCounts -p -s 2 --donotsort -B -t "exon" -g "gene_id" -a ${STARgdir}/${GENCODE} -o $OUTPUT/STAR/Mapped/${SAMPLE}.gn.out $STARgdir/Mapped/${SAMPLE}_mapAligned.sortedByCoord.out.bam &&cut -f1,7 "$OUTPUT/STAR/Mapped/${SAMPLE}.gn.out" > $OUTPUT/STAR/Mapped/${SAMPLE}.gn.out.tab; sed -i 1,2d "$OUTPUT/STAR/Mapped/${SAMPLE}.gn.out.tab"
+${singjob}/featureCounts -p -s 2 --donotsort -B -t "exon" -g "gene_id" -a ${STARgdir}/${GENCODE} -o $OUTPUT/STAR/Mapped/${SAMPLE}.gn.out $STARgdir/Mapped/${SAMPLE}_mapAligned.sortedByCoord.out.bam &&cut -f1,7 "$OUTPUT/STAR/Mapped/${SAMPLE}.gn.out" > $OUTPUT/STAR/Mapped/${SAMPLE}.gn.out.tab; sed -i 1,2d "$OUTPUT/STAR/Mapped/${SAMPLE}.gn.out.tab"
 
-${singjob}/./featureCounts -p -s 2 --donotsort -B -t "exon" -g "exon_id" -a ${STARgdir}/${GENCODE} -o $OUTPUT/STAR/Mapped/${SAMPLE}.ex.out $STARgdir/Mapped/${SAMPLE}_mapAligned.sortedByCoord.out.bam && cut -f1,7 "$OUTPUT/STAR/Mapped/${SAMPLE}.ex.out" > $OUTPUT/STAR/Mapped/${SAMPLE}.ex.out.tab; sed -i 1,2d "$OUTPUT/STAR/Mapped/${SAMPLE}.ex.out.tab" 
+${singjob}/featureCounts -p -s 2 --donotsort -B -t "exon" -g "exon_id" -a ${STARgdir}/${GENCODE} -o $OUTPUT/STAR/Mapped/${SAMPLE}.ex.out $STARgdir/Mapped/${SAMPLE}_mapAligned.sortedByCoord.out.bam && cut -f1,7 "$OUTPUT/STAR/Mapped/${SAMPLE}.ex.out" > $OUTPUT/STAR/Mapped/${SAMPLE}.ex.out.tab; sed -i 1,2d "$OUTPUT/STAR/Mapped/${SAMPLE}.ex.out.tab" 
 
 
 

11151b510c91adb71410676ce0d78ec84fb9aa1e
peterjgiles <79471080+peterjgiles@users.noreply.github.com>
2021-11-12 18:22:41 +0000

    Update STARM2.sh

--- STARM2.sh
+++ STARM2.sh
@@ -9,14 +9,13 @@
 
 module load singularity
 
-
-echo "SAMPLE=${SAMPLE}"
-echo "singjob=${singjob}"
-echo "STARgdir=${STARgdir}"
-echo "JobID=${JobID}"
-echo "F_COUNTS=${F_COUNTS}"
-echo "OUTPUT=${OUTPUT}"
-echo "GENCODE=${GENCODE}"
+#echo "SAMPLE=${SAMPLE}"
+#echo "singjob=${singjob}"
+#echo "STARgdir=${STARgdir}"
+#echo "JobID=${JobID}"
+#echo "F_COUNTS=${F_COUNTS}"
+#echo "OUTPUT=${OUTPUT}"
+#echo "GENCODE=${GENCODE}"
 
 singularity run ${singjob}/STAR-2.7.1a.sif \
   --readFilesCommand zcat \
@@ -30,11 +29,11 @@ singularity run ${singjob}/STAR-2.7.1a.sif \
   --outFileNamePrefix "$STARgdir/Mapped/${SAMPLE}_map"
 
 
-${singjob}/featureCounts -p -s 2 --donotsort -B -t "exon" -g "transcript_id" -a ${STARgdir}/${GENCODE} -o $OUTPUT/STAR/Mapped/${SAMPLE}.tc.out $STARgdir/Mapped/${SAMPLE}_mapAligned.sortedByCoord.out.bam && cut -f1,7 "$OUTPUT/STAR/Mapped/${SAMPLE}.tc.out" > $OUTPUT/STAR/Mapped/${SAMPLE}.tc.out.tab; sed -i 1,2d "$OUTPUT/STAR/Mapped/${SAMPLE}.tc.out.tab"
+singularity run ${singjob}/featurecounts-2.0.3.sif -p -s 2 --donotsort -B -t "exon" -g "transcript_id" -a ${STARgdir}/${GENCODE} -o $OUTPUT/STAR/Mapped/${SAMPLE}.tc.out $STARgdir/Mapped/${SAMPLE}_mapAligned.sortedByCoord.out.bam && cut -f1,7 "$OUTPUT/STAR/Mapped/${SAMPLE}.tc.out" > $OUTPUT/STAR/Mapped/${SAMPLE}.tc.out.tab; sed -i 1,2d "$OUTPUT/STAR/Mapped/${SAMPLE}.tc.out.tab"
 
-${singjob}/featureCounts -p -s 2 --donotsort -B -t "exon" -g "gene_id" -a ${STARgdir}/${GENCODE} -o $OUTPUT/STAR/Mapped/${SAMPLE}.gn.out $STARgdir/Mapped/${SAMPLE}_mapAligned.sortedByCoord.out.bam &&cut -f1,7 "$OUTPUT/STAR/Mapped/${SAMPLE}.gn.out" > $OUTPUT/STAR/Mapped/${SAMPLE}.gn.out.tab; sed -i 1,2d "$OUTPUT/STAR/Mapped/${SAMPLE}.gn.out.tab"
+singularity run ${singjob}/featurecounts-2.0.3.sif -p -s 2 --donotsort -B -t "exon" -g "gene_id" -a ${STARgdir}/${GENCODE} -o $OUTPUT/STAR/Mapped/${SAMPLE}.gn.out $STARgdir/Mapped/${SAMPLE}_mapAligned.sortedByCoord.out.bam &&cut -f1,7 "$OUTPUT/STAR/Mapped/${SAMPLE}.gn.out" > $OUTPUT/STAR/Mapped/${SAMPLE}.gn.out.tab; sed -i 1,2d "$OUTPUT/STAR/Mapped/${SAMPLE}.gn.out.tab"
 
-${singjob}/featureCounts -p -s 2 --donotsort -B -t "exon" -g "exon_id" -a ${STARgdir}/${GENCODE} -o $OUTPUT/STAR/Mapped/${SAMPLE}.ex.out $STARgdir/Mapped/${SAMPLE}_mapAligned.sortedByCoord.out.bam && cut -f1,7 "$OUTPUT/STAR/Mapped/${SAMPLE}.ex.out" > $OUTPUT/STAR/Mapped/${SAMPLE}.ex.out.tab; sed -i 1,2d "$OUTPUT/STAR/Mapped/${SAMPLE}.ex.out.tab" 
+singularity run ${singjob}/featurecounts-2.0.3.sif -p -s 2 --donotsort -B -t "exon" -g "exon_id" -a ${STARgdir}/${GENCODE} -o $OUTPUT/STAR/Mapped/${SAMPLE}.ex.out $STARgdir/Mapped/${SAMPLE}_mapAligned.sortedByCoord.out.bam && cut -f1,7 "$OUTPUT/STAR/Mapped/${SAMPLE}.ex.out" > $OUTPUT/STAR/Mapped/${SAMPLE}.ex.out.tab; sed -i 1,2d "$OUTPUT/STAR/Mapped/${SAMPLE}.ex.out.tab" 
 
 
 

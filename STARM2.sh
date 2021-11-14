7e3f75627fb44ff92b460d5eadbff1dae1396235
peterjgiles <79471080+peterjgiles@users.noreply.github.com>
2021-11-14 18:47:39 +0000

    Update STARM2.sh

--- STARM2.sh
+++ STARM2.sh
@@ -36,6 +36,5 @@ singularity run ${singjob}/featurecounts-2.0.3.sif -p -s 2 --donotsort -B -t "ex
 singularity run ${singjob}/featurecounts-2.0.3.sif -p -s 2 --donotsort -B -t "exon" -g "exon_id" -a ${STARgdir}/${GENCODE} -o $OUTPUT/STAR/Mapped/${SAMPLE}.ex.out $STARgdir/Mapped/${SAMPLE}_mapAligned.sortedByCoord.out.bam && cut -f1,7 "$OUTPUT/STAR/Mapped/${SAMPLE}.ex.out" > $OUTPUT/STAR/Mapped/${SAMPLE}.ex.out.tab; sed -i 1,2d "$OUTPUT/STAR/Mapped/${SAMPLE}.ex.out.tab" 
 
 
-
 mv $STARgdir/Mapped/*.final.out $OUTPUT/logs/STAR
 echo "Mapping finished" >>/scratch/$USER/$JobID/${JobID}.log

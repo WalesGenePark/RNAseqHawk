cdddb9513aba97af78e983cd5927bb83afdda743
peterjgiles <79471080+peterjgiles@users.noreply.github.com>
2021-11-19 12:14:20 +0000

    Update QualTrim2.sh

--- QualTrim2.sh
+++ QualTrim2.sh
@@ -2,10 +2,10 @@
 
 module load singularity
 singularity run ${SINGDIR}/fastp-v0.23.1.sif \
-    --in1 ${TMPDIR}/${SamplesRaw}_F.fastq.gz \
-    --in2 ${TMPDIR}/${SamplesRaw}_R.fastq.gz \
-    --out1 ${OUTPUT}/trim/${SamplesRaw}_trimmed_F.fq.gz \
-    --out2 ${OUTPUT}/trim/${SamplesRaw}_trimmed_R.fq.gz 
+    --in1 ${TMPDIR}/${SampleRaw}_F.fastq.gz \
+    --in2 ${TMPDIR}/${SampleRaw}_R.fastq.gz \
+    --out1 ${OUTPUT}/trim/${SampleRaw}_trimmed_F.fq.gz \
+    --out2 ${OUTPUT}/trim/${SampleRaw}_trimmed_R.fq.gz 
     --thread ${SLURM_CORES} -h \
-    ${OUTPUT}/logs/${SamplesRaw}_fastp.html -j \
-    ${OUTPUT}/logs/${SamplesRaw}_fastp.json
+    ${OUTPUT}/logs/${SampleRaw}_fastp.html -j \
+    ${OUTPUT}/logs/${SampleRaw}_fastp.json

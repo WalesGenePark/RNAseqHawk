70f99294568a5fae14d39e4eadf14b0ca5283472
peterjgiles <79471080+peterjgiles@users.noreply.github.com>
2021-11-19 14:05:35 +0000

    Update mergetrim.sh

--- mergetrim.sh
+++ mergetrim.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cat ${OUTPUT}/trim/${SAMPLE}_*F.fq.gz >> ${STARgdir}/reads/${SAMPLE}_M_F.fq.gz;
-cat ${OUTPUT}/trim/trim/${SAMPLE}_*R.fq.gz >> ${STARgdir}/reads/${SAMPLE}_M_R.fq.gz;
+cat ${OUTPUT}/trim/${SAMPLE}_*R.fq.gz >> ${STARgdir}/reads/${SAMPLE}_M_R.fq.gz;
